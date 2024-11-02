4864d9c25cee50748a2ed71cbe82a79a90b03269
JADELKA / JDT <166043734+NioyJadelkaFp@users.noreply.github.com>
2024-11-02 16:29:31 -0300

    listo sale la ultima entrada de hoy

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Establecer la clave secreta
 app.secret_key = 'tu_clave_secreta_unica'
 
-def get_db_connection():
+def Conexiondb():
     try:
         conn = pymysql.connect(
             host='by8ekzvhusvvn2yqc71b-mysql.services.clever-cloud.com',
@@ -21,22 +21,19 @@ def get_db_connection():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    conn = get_db_connection()
+    conn = Conexiondb()
     if not conn:
         flash("No se pudo conectar a la base de datos.", "danger")
         return render_template('index.html', asistencias_hoy=[], busqueda='')
 
-    asistencias_hoy = []
+    asistencias_hoy2 = []
     busqueda = ''
 
     if request.method == 'POST':
         busqueda = request.form.get('busqueda', '').strip()
-        
-        # Verificar si hay un valor en la búsqueda
-        if busqueda:  
+        if busqueda:
             try:
                 with conn.cursor() as cursor:
-                    # Consulta para entradas (asistencias)
                     cursor.execute(
                         """SELECT e.nombre, e.codigo, e.nie, ent.id_entrada, ent.fecha, ent.hora 
                         FROM entrada ent
@@ -45,10 +42,10 @@ def index():
                         ORDER BY ent.fecha DESC, ent.hora DESC""",
                         ('%' + busqueda + '%', '%' + busqueda + '%')
                     )
-                    asistencias_hoy = cursor.fetchall()
+                    asistencias_hoy2 = cursor.fetchall()
                     
                     # Si no hay resultados, mostrar un mensaje
-                    if not asistencias_hoy:
+                    if not asistencias_hoy2:
                         flash("No se encontraron resultados para la búsqueda.", "warning")
 
             except Exception as e:
@@ -59,7 +56,7 @@ def index():
         else:
             flash("Por favor, ingresa un NIE o Código para buscar.", "warning")
 
-    return render_template('index.html', asistencias_hoy=asistencias_hoy, busqueda=busqueda)
+    return render_template('index.html', asistencias_hoy=asistencias_hoy2, busqueda=busqueda)
 
 if __name__ == '__main__':
     app.run(debug=True)
